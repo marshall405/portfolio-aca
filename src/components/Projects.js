57dcf88ddb7929958690e3b59261186bd535c87e
Marshall Slemp <marshall.slemp@gmail.com>
2018-09-18 22:17:30 -0500

    add another project, todolist

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,17 +4,37 @@ import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 
 import colorMashImg from '../images/colormash.png';
+import todolistImg from '../images/todolist.png';
+
+
+const tileData = [
+    {
+        img : colorMashImg,
+        title : 'Color Mash',
+        src : '../../public/colorMash/index.html'
+    },
+    {
+        img : todolistImg,
+        title : 'To Do List',
+        src : ''
+    }
+]
 export class Projects extends React.Component {
     render(){
         return (
             <div className='projects-container'>
                 <h1> Projects </h1>
                 <div className='projects'>
-                    <GridList cols={1} style={{width: 400, height: 300}}>
-                        <GridListTile style={{height: 'auto'}}>
-                            <GridListTileBar title='Color Mash'/>
-                            <a href='https://htmlpreview.github.io/?https://github.com/marshall405/colorMash/blob/master/index.html'><img src={colorMashImg} alt='color mash' /></a>
-                        </GridListTile>
+
+                    <GridList cols={1} style={{width: 400, height: 350}}>
+                        {tileData.map(tile => (
+                            
+                            <GridListTile key={tile.img}>
+                                <a href={tile.src}><img src={tile.img} alt={tile.title}/></a>
+                                <GridListTileBar title={tile.title}/>
+                            </GridListTile>
+                        
+                        ))}
                     </GridList>
                 </div>
             </div>
