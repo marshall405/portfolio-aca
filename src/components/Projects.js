ae7688c1a5150eb6c12326e526b4eaa64a68f5ba
Marshall Slemp <marshall.slemp@gmail.com>
2018-10-12 00:02:55 -0500

    redesign projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LabelIcon from '@material-ui/icons/Label';
 
-import colorMashImg from '../images/colormash.png';
-import todolistImg from '../images/todolist.png';
 
-
-const tileData = [
+const projects = [
     {
-        img : colorMashImg,
         title : 'Color Mash',
+        description: 'One of the first "programs" I wrote! Color Mash will let you combine two colors together and see the result.',
         href : 'https://htmlpreview.github.io/?https://github.com/marshall405/colorMash/blob/master/index.html'
     },
     {
-        img : todolistImg,
         title : 'To Do List',
+        description: 'My first ReactJS App. A simple to do list, lets you add, edit, mark as complete or delete items.',
         href : 'https://marshall405.github.io/todolist/'
+    },
+    {
+        title: 'Clock',
+        description: 'Clock app built with just ReactJS. Can be analog or digital, just click on the clock to switch between the two',
+        href: 'https://marshall405.github.io/clock-react-app/'
+    },
+    {
+        title: 'Weather App',
+        description: 'Created a simple weather app using "openweathermap\'s" API. Again, built using ReactJS',
+        href: 'https://marshall405.github.io/another-weather-app/'
     }
 ]
 export class Projects extends React.Component {
@@ -26,16 +35,22 @@ export class Projects extends React.Component {
                 <h1> Projects </h1>
                 <div className='projects'>
 
-                    <GridList cols={1} style={{width: 400, height: 350}}>
-                        {tileData.map(tile => (
+                    
+                        {projects.map((project,index) => (
                             
-                            <GridListTile key={tile.img}>
-                                <a href={tile.href}><img src={tile.img} alt={tile.title}/></a>
-                                <GridListTileBar title={tile.title}/>
-                            </GridListTile>
-                        
+                            <List key={index}>
+                                <a href={project.href}>
+                                    <ListItem button divider>
+                                        <ListItemIcon>
+                                            <LabelIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary={project.title} secondary={project.description}/> 
+                                    </ListItem>
+                                </a>
+                            </List>
                         ))}
-                    </GridList>
+                        
+                    
                 </div>
             </div>
         )
