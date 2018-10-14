8071a279299820ff0e6a670b0b30484277662e48
Marshall Slemp <marshall.slemp@gmail.com>
2018-10-14 12:32:14 -0500

    add X to contact me modal

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,30 @@
 import React from 'react';
 
-export class Contact extends React.Component {
 
+const XDivStyle = {
+    textAlign: 'right',
+    padding: 10
+}
+export class Contact extends React.Component {
 
 
 
     render() {
         return (
             <div>
-            <div id='contact' onClick={() => this.props.onClick()}> 
+            <div  id='contact' onClick={() => this.props.onClick()}> 
             </div>
             <div className='contact-container'>
-                <h1> Contact Me </h1>
-                <p> 405-919-2960 </p>
-                <p><a href='mailto:marshall.slemp@gmail.com'>marshall.slemp@gmail.com</a></p>
-                <h3> Current Location </h3>
-                <p> Round Rock, Texas </p>
+                <div style={XDivStyle}> 
+                    <h2 className='contact-X' onClick={() => this.props.onClick()}>X</h2>
+                </div>
+                <div className='center'>
+                    <h1> Contact Me </h1>
+                    <p> 405-919-2960 </p>
+                    <p><a href='mailto:marshall.slemp@gmail.com'>marshall.slemp@gmail.com</a></p>
+                    <h3> Current Location </h3>
+                    <p> Round Rock, Texas </p>
+                </div>
             </div>
         </div>
         )
