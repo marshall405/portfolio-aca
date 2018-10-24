9a99d726e103434ba841f26462983604f641a780
Marshall Slemp <marshall.slemp@gmail.com>
2018-10-23 21:34:08 -0500

    adjust arrow upward size according to screen width

--- src/components/BackToTop.js
+++ src/components/BackToTop.js
@@ -4,15 +4,12 @@ import ArrowUpward from '@material-ui/icons/ArrowUpward';
 const style = {
     zIndex: 102,
     position: 'fixed',
-    bottom: 35,
-    right: 40
 }
 
-
-export const BackToTop = () => {
-    
+export const BackToTop = (props) => {
+    let fontSize = props.width > 510 ? 50 : 25;
     return (
-        <a href="#top" style={style} className='backToTop'> <ArrowUpward style={{fontSize: 50}}/> </a>
+        <a href="#top" style={style} className='backToTop'> <ArrowUpward style={{fontSize: fontSize}}/> </a>
     )
 
 
