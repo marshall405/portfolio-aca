e186c95ce2f10d56cad8472a0c6a87885fb57c87
Marshall Slemp <marshall.slemp@gmail.com>
2018-12-29 02:41:54 -0600

    add resume pdf to website

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import CaboPic from '../images/cabo.jpg';
 import CartoonPic from '../images/cartoonPic.png';
+import Resume from '../assets/resume.pdf';
 // import { Timeline } from '../components/Timeline';
 
 const greetingStyle = {
@@ -11,6 +12,13 @@ const greetingStyle = {
     marginBottom: 10
 
 }
+const aStyle = {
+    textDecoration: 'underline'
+}
+const divAStyle = {
+    padding: 15,
+    textAlign: 'center'
+}
 export class About extends React.Component {
     render() {
         return (
@@ -18,7 +26,7 @@ export class About extends React.Component {
                <h1> About Me </h1>
                <div className='inner-about-container'>
                     {/* <img className='profile-pic' src={CaboPic} alt='cabo :)'/> */}
-                    <img className='profile-pic' src={CartoonPic} alt='cabo :)'/>
+                    <img className='profile-pic' src={CartoonPic} alt='animated profile'/>
                     <div>
                         <p style={greetingStyle}> Hello, my name is Marshall and I am a self-taught web developer!</p>
                         <p style={greetingStyle}> Years ago I attended Oklahoma State University in Stillwater, Oklahoma where I majored in General Business. </p>
@@ -27,6 +35,7 @@ export class About extends React.Component {
                         <p style={greetingStyle}> The first thing I built was a simple website using only HTML and CSS, I sent that website back to the 90's. </p>
                         <p style={greetingStyle}> My most recent project is a Full-Stack application where you can 'post a comment'. I used HTML, CSS and ReactJS for the front end. NodeJS, Express and MySQL for the backend. At the end of every night all messages get deleted. </p>
                         <p style={greetingStyle}> Checkout some of my projects furthur down the page. Or click <a href="#projects" style={{borderBottom: '1px solid black'}}>here!</a></p>
+                        <div style={divAStyle}><a href={Resume} style={aStyle}> View Resume </a></div>
                         {/* <Timeline windowWidth={this.props.windowWidth}/> */}
                     </div>
                 </div>
