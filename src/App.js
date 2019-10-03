53f338029115732d80732145a9f8573d12cfd345
Marshall Slemp <marshall.slemp@gmail.com>
2019-10-03 01:38:54 -0500

    add slider to adjust canvas colors

--- src/App.js
+++ src/App.js
@@ -22,14 +22,20 @@ class App extends Component {
     super(props);
     this.state = {
       canvas: 0,
-      canvasColor: 'white'
+      canvasColor: 'rgb(255,255,255)'
     }
     this.changeCanvas = this.changeCanvas.bind(this);
     this.changeCanvasColor = this.changeCanvasColor.bind(this);
+    this.handlerSlider = this.handlerSlider.bind(this);
   }
   componentDidMount() {
     SafariSmoothScroll();
     handleScroll();
+
+
+    const slider = document.getElementById('slider');
+    slider.addEventListener('input', this.handlerSlider);
+
     // wake up contact server 
     fetch('https://lit-mountain-55987.herokuapp.com/wakeup');
   }
@@ -47,19 +53,43 @@ class App extends Component {
     })
   }
   changeCanvasColor() {
-    let color = this.state.canvasColor === 'white' ? 'black' : 'white';
+    let color = this.state.canvasColor === 'rgb(255,255,255)' ? 'rgb(0,0,0)' : 'rgb(255,255,255)';
     let button = document.getElementById('changeCanvasColor');
-    button.innerText = color === 'white' ? 'night' : 'day';
+    button.innerText = color === 'rgb(255,255,255)' ? 'dark' : 'light';
     this.setState({
       canvasColor: color
     })
+  }
+  handlerSlider(e) {
+    const red = document.getElementById('red').value;
+    const green = document.getElementById('green').value;
+    const blue = document.getElementById('blue').value;
+    let colorRange = `rgb(${red},${green},${blue})`
+    this.setState({
+      canvasColor: colorRange
+    })
+
   }
   render() {
     return (
       <div className='container'>
         {this.renderCanvas()}
+        <div id='slider' className='colorRange'>
+          <div>
+            <label htmlFor='red'> r </label>
+            <input id='red' type='range' name='red' min='0' max='255' step='1' defaultValue='255' />
+          </div>
+          <div>
+            <label htmlFor='green'> g </label>
+            <input id='green' type='range' name='green' min='0' max='255' step='1' defaultValue='255' />
+          </div>
+          <div >
+            <label htmlFor='blue'> b </label>
+            <input id='blue' type='range' name='blue' min='0' max='255' step='1' defaultValue='255' />
+          </div>
+        </div>
         <button id='changeCanvas' onClick={this.changeCanvas}> > </button>
-        <button id='changeCanvasColor' onClick={this.changeCanvasColor}> night </button>
+        <button id='changeCanvasColor' onClick={this.changeCanvasColor}> dark </button>
         <Heading />
         <Jumbo />
 
