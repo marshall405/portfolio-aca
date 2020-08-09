d01e6e85cbfb885ced6b0605c2e036329109cc27
marshall405 <marshall.slemp@gmail.com>
2020-08-09 12:33:58 -0500

    changed about and added technologies

--- src/components/About.js
+++ src/components/About.js
@@ -8,10 +8,46 @@ export class About extends React.Component {
         return (
             <div className='section' id='about'>
                 <h2> ABOUT </h2>
-                <p> Hello, my name is Marshall. I am a Web Developer living in Round Rock, TX.</p>
-                <p> I will be attending <a href='https://flatironschool.com/' target='blank'><strong>FLATIRON's</strong></a> Software Engineering Bootcamp starting April 20, 2020! </p>
+                <p> Hello, my name is Marshall. I am a Web Developer living in Georgetown, TX.</p>
+                <p> I recently graduated <a href='https://flatironschool.com/' target='blank'><strong>FLATIRON's</strong></a> Software Engineering Bootcamp! </p>
                 <div className='about-image-container'>
                     <img id='profileImg' src={Cabo} alt='me in cabo'></img>
+                </div>
+
+                <h4>Technologies</h4>
+
+                <div className="skills-container">
+                    <div className="skill">
+                        <h5> Programming Languages </h5>
+                        <ul>
+                            <li> JavaScript </li>
+                            <li> Ruby </li>
+                        </ul>
+                    </div>
+
+                    <div className="skill">
+
+                        <h5> Libraries / Frameworks </h5>
+
+                        <ul>
+                            <li>Rails</li>
+                            <li>React</li>
+                            <li></li>
+                        </ul>
+
+                    </div>
+
+                    <div className="skill">
+
+                        <h5> Databases </h5>
+
+                        <ul>
+                            <li>SQL</li>
+                            <li>PostgreSQL</li>
+                        </ul>
+
+                    </div>
+
                 </div>
             </div>
         )
