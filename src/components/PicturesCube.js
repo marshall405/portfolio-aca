a7b1cf2f15faf8c99423480941165a474f09a4ca
marshall405 <marshall.slemp@gmail.com>
2020-11-09 14:34:32 -0600

    adds scroll on hover for about images, hides arrows when min-width > 700px

--- src/components/PicturesCube.js
+++ src/components/PicturesCube.js
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types'
 
 
 const img = {
-    width: '50%',
+    width: '250px',
     maxWidth: '260px',
     minWidth: '200px',
     height: '96%',
+    margin: '5px',
     borderRadius: '5px',
     boxShadow: '2px 2px 5px 1px rgb(58, 58, 58)',
-    position: 'absolute'
+
 }
 const galleryContainer = {
     display: 'flex',
@@ -24,10 +25,10 @@ const galleryContainer = {
 }
 const imgContainer = {
     display: 'flex',
-    overflow: 'hidden',
-    position: 'relative',
-    justifyContent: 'space-evenly',
+    flexWrap: 'nowrap',
+    overflowX: 'scroll',
     alignItems: 'center',
+    justifyContent: 'space-between',
     height: '100%',
     width: '70%',
 }
@@ -37,52 +38,56 @@ export default class PicturesCube extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            index: 0
+            index: 0,
         }
+
     }
 
-    componentDidMount() {
-        setInterval(() => {
-            this.OnRightClick()
-        }, 5000)
+    scrollRight = () => {
+        this.setState({ right: !this.state.right })
     }
-    OnLeftClick = () => {
-        let index = this.state.index
-        if (index - 1 < 0) {
-            index = this.props.pictures.length
-        }
-        this.setState({
-            index: index -= 1
-        })
+    scrollLeft = () => {
+        this.setState({ left: !this.state.left })
     }
-    OnRightClick = () => {
-        let index = this.state.index
-        if (index + 1 >= this.props.pictures.length) {
-            index = -1
+
+    scroll = () => {
+        if (this.state.right) {
+            setTimeout(() => {
+                document.getElementById('imageContainer').scrollLeft += 20
+                if (this.state.right) {
+                    this.scroll()
+                }
+            }, 20)
         }
-        this.setState({
-            index: index += 1
-        })
+        if (this.state.left) {
+            setTimeout(() => {
+                document.getElementById('imageContainer').scrollLeft -= 20
+                if (this.state.left) {
+                    this.scroll()
+                }
+            }, 20)
+        }
+
     }
     render() {
 
         return (
             <div style={galleryContainer} className="image-gallery">
-                <i className="fas fa-arrow-left arrow" onClick={this.OnLeftClick}></i>
-
-                <div style={imgContainer}>
+                <i className="fas fa-arrow-left arrow" onMouseEnter={this.scrollLeft} onMouseLeave={this.scrollLeft}></i>
+                {
+                    this.scroll()
+                }
+                <div id="imageContainer" style={imgContainer}>
                     {
                         this.props.pictures.map((picture, index) => {
-                            if (this.state.index === index) {
-                                return <img key={index} className="show" style={img} src={picture} alt="me" />
-                            } else {
-                                return <img key={index} className="hide" style={img} src={picture} alt="me" />
-                            }
+
+                            return <img key={index} style={img} src={picture} alt="me" />
+
                         })
                     }
                 </div>
 
-                <i className="fas fa-arrow-right arrow" onClick={this.OnRightClick}></i>
+                <i className="fas fa-arrow-right arrow" onMouseEnter={this.scrollRight} onMouseLeave={this.scrollRight}></i>
 
 
             </div>
