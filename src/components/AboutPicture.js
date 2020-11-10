0e1b066de2c652aba7941b4e46cea101216356b7
marshall405 <marshall.slemp@gmail.com>
2020-11-10 10:20:18 -0600

    adds single profile img to about section

--- src/components/AboutPicture.js
+++ src/components/AboutPicture.js
@@ -4,20 +4,10 @@ import PropTypes from 'prop-types'
 import '../styles/aboutPicture.css'
 
 export default class AboutPicture extends Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            left: 0,
-        }
-
-    }
-
-
     render() {
         return (
             <div className="about-image">
-                <img style={{ left: this.state.left }} src={this.props.picture} alt="A closeup of ME!" />
+                <img src={this.props.picture} alt="A closeup of ME!" />
             </div>
         )
 
